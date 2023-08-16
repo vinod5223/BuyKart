6faa1321aa2793213b9788737c2972a8cd223ff6
USER <USER@DESKTOP-ORKTBJG>
2023-08-16 20:26:42 +0530

    Branch_02

---
+++ src/test/java/practiceTestNG/ResumeBuild.java
@@ -0,0 +1,10 @@
+package practiceTestNG;
+
+import org.testng.annotations.Test;
+
+public class ResumeBuild {
+	@Test
+	public void changed_Module1Test() {
+		System.out.println("this is the newly added feature");
+	}
+}
