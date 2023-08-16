c799c0d12dd043effdca8076ccbcd6a8f5126f9e
USER <USER@DESKTOP-ORKTBJG>
2023-08-16 18:54:46 +0530

    NewChanged Features

---
+++ src/test/java/practiceTestNG/ChangedModuleTest.java
@@ -0,0 +1,11 @@
+package practiceTestNG;
+
+import org.testng.annotations.Test;
+
+public class ChangedModuleTest {
+
+	@Test
+	public void changed_ModuleTest() {
+		System.out.println("this is the newly added feature");
+	}
+}
